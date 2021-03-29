5367164725d793ab6447df8c03ff50dbc3586c1e
Shvana <htsunemikuv9@gmail.com>
2021-03-29 16:56:21 -0600

    portfolio section

--- js/j-main.js
+++ js/j-main.js
@@ -148,7 +148,6 @@
         })
     }
 
-
     /**
      * Porfolio isotope and filter
      */
@@ -160,9 +159,9 @@
                 layoutMode: 'fitRows'
             });
 
-            let portfolioFilters = select('#portfolio-flters li', true);
+            let portfolioFilters = select('#portfolio-filters li', true);
 
-            on('click', '#portfolio-flters li', function(e) {
+            on('click', '#portfolio-filters li', function(e) {
                 e.preventDefault();
                 portfolioFilters.forEach(function(el) {
                     el.classList.remove('filter-active');
